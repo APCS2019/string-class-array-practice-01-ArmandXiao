e73c354a41b3a7a5a12d27006bec14746f4c812c
ArmandXiao <45375764+ArmandXiao@users.noreply.github.com>
2018-12-04 09:30:27 +0800

    Final

--- 20181204-ArmandXiao.java
+++ 20181204-ArmandXiao.java
@@ -11,7 +11,7 @@ public class HiddenWord{
       for( int i = 0; i< s.length(); i++){
         if(s.substring(i,i+1).equals(current.substring(i,i+1)){
            temp += current.substring(i,i+1);
-        else if(s.substring(i,i+1).equals(current.substring(i,i+1) && s.substring(i, i+1).indexOf(current) != -1)
+        else if(current.indexOf(s.substring(i,i+1))
            temp += "+";
       else
         temp += "*";
