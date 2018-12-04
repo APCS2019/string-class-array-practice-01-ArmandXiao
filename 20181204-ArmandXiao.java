9ce1770281c94f1fd90afd8941b5a7a8c0e7f916
ArmandXiao <45375764+ArmandXiao@users.noreply.github.com>
2018-12-04 09:35:45 +0800

    Final_final

--- 20181204-ArmandXiao.java
+++ 20181204-ArmandXiao.java
@@ -7,20 +7,14 @@ public class HiddenWord{
   }  
   public String getHint(String s){
       String temp = "";
-      boolean a = false;
+     
       for( int i = 0; i< s.length(); i++){
-          if(!a && s.substring(i,i+1).equals(current.substring(i,i+1))
-              a = true;
-      }
-      if(a){
         if(s.substring(i,i+1).equals(current.substring(i,i+1)){
            temp += current.substring(i,i+1);
-        else
+        else if(current.indexOf(s.substring(i,i+1)!=-1)
            temp += "+";
-      }
       else
         temp += "*";
-      return temp;  
-           
+      return temp;           
   }
  }
