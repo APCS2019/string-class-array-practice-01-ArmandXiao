40751c0574c8c359cdbaa88c9c79231fe0bd3722
ArmandXiao <45375764+ArmandXiao@users.noreply.github.com>
2018-12-04 09:11:22 +0800

    Create 20181204-ArmandXiao.java

---
+++ 20181204-ArmandXiao.java
@@ -0,0 +1,25 @@
+//pastPaper-2015
+//key idea: String, Class
+public class HiddenWord{
+  private String current;
+  public HiddenWord(String a){
+    current = a;
+  }  
+  public String getHint(String s){
+      String temp = "";
+      boolean a = false;
+      for( int i = 0; i< s.length(); i++){
+          if(!a && s.substring(i,i+1).equals(current.substring(i,i+1))
+              a = true;
+      }
+      if(a){
+        if(s.substring(i,i+1).equals(current.substring(i,i+1)){
+           temp += current.substring(i,i+1);
+        else
+           temp += "+";
+      }
+      else
+        temp += "*";
+      return temp;
+  }
+ }
